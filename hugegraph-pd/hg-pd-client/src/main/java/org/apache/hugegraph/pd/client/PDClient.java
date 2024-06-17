39ec4c0a8be7e39c970f95bffae4a653953b4eb3
alan578.zhao <956322745@qq.com>
2024-06-17 16:11:30 +0800

    1、内存泄露修复，2、缓存失效问题修复，3、支持关闭auto_compaction

--- hugegraph-pd/hg-pd-client/src/main/java/org/apache/hugegraph/pd/client/PDClient.java
+++ hugegraph-pd/hg-pd-client/src/main/java/org/apache/hugegraph/pd/client/PDClient.java
@@ -461,7 +461,10 @@ public class PDClient {
     public KVPair<Metapb.Partition, Metapb.Shard> getPartition(String graphName, byte[] key) throws
                                                                                              PDException {
         // 先查cache，cache没有命中，在调用PD
-        KVPair<Metapb.Partition, Metapb.Shard> partShard = cache.getPartitionByKey(graphName, key);
+        int code = PartitionUtils.calcHashcode(key);
+        KVPair<Metapb.Partition, Metapb.Shard> partShard = this.getPartitionByCode(graphName, code);
+        // 先查cache，cache没有命中，在调用PD
+//        KVPair<Metapb.Partition, Metapb.Shard> partShard = cache.getPartitionByKey(graphName, key);
         partShard = getKvPair(graphName, key, partShard);
         return partShard;
     }
